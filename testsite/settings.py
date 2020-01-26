f9dad4d7585790b2485af6e0f3aa2fc9b7f01980
Krishnendu Chatterjee <Krishnenduchatterjee25@gmail.com>
2020-01-27 01:48:17 +0530

    first commit

--- testsite/settings.py
+++ testsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +143,4 @@ DEFAULT_FROM_EMAIL = 'My Site Team <noreply@mysite.com>'
 
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL='/login'
+django_heroku.settings(locals())
